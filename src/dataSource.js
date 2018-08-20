30a38b998e93da03d43a74c45f5178266aeaf641
Bhargav Krishna <bhargavkrishna@admins-MacBook-Pro.local>
2018-08-20 01:42:43 -0700

    Creating a dataSource setting

--- src/dataSource.js
+++ src/dataSource.js
@@ -1,14 +1,17 @@
 import _ from "lodash"
 import faker from "faker"
+import { MAX_CARDS_PER_PAGE, MAX_PAGES } from "./settings"
 
 const createDataSource = () =>
-  _.map(_.range(1, 200), id => ({
+  _.map(_.range(1, MAX_PAGES), id => ({
     id: _.uniqueId(),
-    cards: _.map(_.range(1, 6), id => ({
+    cards: _.map(_.range(1, MAX_CARDS_PER_PAGE), id => ({
       id: _.uniqueId(),
       card: faker.helpers.createCard(),
       image: faker.image.avatar()
     }))
   }))
+
+console.log(MAX_CARDS_PER_PAGE, MAX_PAGES)
 
 export default createDataSource
--- src/settings.js
+++ src/settings.js
@@ -5,3 +5,6 @@ export const MAX_PAGE_BUFFER = 20
 
 export const VIEWPORT_HEIGHT = window.screen.availHeight - 400
 export const FF_MULTIPLIER = VIEWPORT_HEIGHT * 0.05
+
+export const MAX_PAGES = 4
+export const MAX_CARDS_PER_PAGE = 10
