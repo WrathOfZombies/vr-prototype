e1be96497ddd944b21b7ad7591e9b93c54e9f2e2
WrathOfZombies <bhargav.krishna@outlook.com>
2018-08-22 23:59:56 -0700

    Adding basic interfaces and settings

--- src/index.tsx
+++ src/index.tsx
@@ -6,23 +6,48 @@ import { MAX_CARDS_PER_PAGE, MAX_PAGES } from "./settings";
 import VirtualList from "./VirtualList";
 
 const createDataSource = () =>
-  _.map(_.range(1, MAX_PAGES), id => ({
-    id: _.uniqueId(),
-    cards: _.map(_.range(1, _.random(0, MAX_CARDS_PER_PAGE, false)), id => ({
-      id: _.uniqueId(),
-      card: faker.helpers.createCard(),
-      image: faker.image.avatar()
-    }))
-  }));
+  _.map(
+    _.range(1, MAX_PAGES),
+    id =>
+      ({
+        id,
+        cards: _.map(_.range(1, _.random(1, MAX_CARDS_PER_PAGE)), () => ({
+          id: _.uniqueId(),
+          card: faker.helpers.createCard(),
+          image: faker.image.avatar()
+        }))
+      } as any)
+  );
 
 const dataSource = createDataSource();
 
-const getPrevPage = currentPage => Promise.resolve(dataSource.pop());
+const getPrevPage = currentPage => {
+  if (!currentPage) {
+    return Promise.resolve(dataSource[0]);
+  } else {
+    const id = currentPage.id - 1 || dataSource.length;
+    return Promise.resolve(dataSource[id - 1]);
+  }
+};
 
-const getNextPage = currentPage => Promise.resolve(dataSource.pop());
+const getNextPage = currentPage => {
+  if (!currentPage) {
+    return Promise.resolve(dataSource[0]);
+  } else {
+    const id = currentPage.id === dataSource.length ? 0 : currentPage.id;
+    return Promise.resolve(dataSource[id]);
+  }
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <VirtualList getNextPage={getNextPage} getPrevPage={getPrevPage} />,
+  <VirtualList
+    getNextPage={getNextPage}
+    getPrevPage={getPrevPage}
+    settings={{
+      debug: false,
+      startBottomUp: true
+    }}
+  />,
   rootElement
 );
